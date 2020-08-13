f8f72654b990807a06c6e6afe307b135035ab165
wjxor <shko940575@gmail.com>
2020-08-14 03:16:03 +0900

    글 등록기능 구현

--- src/com/sbs/java/blog/servlet/ArticleDoWriteServlet.java
+++ src/com/sbs/java/blog/servlet/ArticleDoWriteServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -18,20 +19,50 @@ public class ArticleDoWriteServlet extends HttpServlet {
 		response.setContentType("text/html; charset=UTF-8");
 
 		String url = "jdbc:mysql://localhost:3306/blog?serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true";
-		String user = "";
+		String user = "root";
 		String password = "";
 		String driverName = "com.mysql.cj.jdbc.Driver";
 
-		Connection connection;
+		String title = request.getParameter("title");
+		String body = request.getParameter("body");
+
+		Connection connection = null;
+		Statement stmt = null;
+
+		String sql = "";
+		sql += String.format("INSERT INTO article ");
+		sql += String.format("SET regDate = NOW()");
+		sql += String.format(", updateDate = NOW()");
+		sql += String.format(", title = '%s'", title);
+		sql += String.format(", body = '%s'", body);
 
 		try {
 			Class.forName(driverName);
 			connection = DriverManager.getConnection(url, user, password);
-			response.getWriter().append("연결되었습니다.");
+			stmt = connection.createStatement();
+			int affectedRows = stmt.executeUpdate(sql);
+
+			response.getWriter().append(affectedRows + "개의 데이터가 추가됨");
 		} catch (SQLException e) {
 			System.err.printf("[SQL 예외] : %s\n", e.getMessage());
 		} catch (ClassNotFoundException e) {
 			System.err.printf("[드라이버 클래스 로딩 예외] : %s\n", e.getMessage());
+		} finally {
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					System.err.printf("[SQL 예외, connection 닫기] : %s\n", e.getMessage());
+				}
+			}
+
+			if (stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException e) {
+					System.err.printf("[SQL 예외, stmt 닫기] : %s\n", e.getMessage());
+				}
+			}
 		}
 	}
 
