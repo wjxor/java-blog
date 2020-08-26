8f9a309c6651f851c51b4dcd08bc7495f0e8cafe
wjxor <shko940575@gmail.com>
2020-08-27 03:04:10 +0900

    미래를 위한 큰그림을 위해서 attr 내부구조 변경

--- src/com/sbs/java/blog/dao/AttrDao.java
+++ src/com/sbs/java/blog/dao/AttrDao.java
@@ -13,11 +13,11 @@ public class AttrDao {
 		this.dbConn = dbConn;
 	}
 
-	public int setValue(String name, String value) {
+	public int setValue(String relTypeCode, int relId, String typeCode, String type2Code, String value) {
 		SecSql sql = new SecSql();
 
-		sql.append("INSERT INTO attr (regDate, updateDate, `name`, `value`)");
-		sql.append("VALUES (NOW(), NOW(), ?, ?)", name, value);
+		sql.append("INSERT INTO attr (regDate, updateDate, `relTypeCode`, `relId`, `typeCode`, `type2Code`, `value`)");
+		sql.append("VALUES (NOW(), NOW(), ?, ?, ?, ?, ?)", relTypeCode, relId, typeCode, type2Code, value);
 		sql.append("ON DUPLICATE KEY UPDATE");
 		sql.append("updateDate = NOW()");
 		sql.append(", `value` = ?", value);
@@ -25,31 +25,43 @@ public class AttrDao {
 		return DBUtil.update(dbConn, sql);
 	}
 
-	public Attr get(String name) {
+	public Attr get(String relTypeCode, int relId, String typeCode, String type2Code) {
 		SecSql sql = new SecSql();
 
 		sql.append("SELECT *");
 		sql.append("FROM attr");
-		sql.append("WHERE `name` = ?", name);
+		sql.append("WHERE 1");
+		sql.append("AND `relTypeCode` = ?", relTypeCode);
+		sql.append("AND `relId` = ?", relId);
+		sql.append("AND `typeCode` = ?", typeCode);
+		sql.append("AND `type2Code` = ?", type2Code);
 
 		return new Attr(DBUtil.selectRow(dbConn, sql));
 	}
 
-	public String getValue(String name) {
+	public String getValue(String relTypeCode, int relId, String typeCode, String type2Code) {
 		SecSql sql = new SecSql();
 
 		sql.append("SELECT `value`");
 		sql.append("FROM attr");
-		sql.append("WHERE `name` = ?", name);
+		sql.append("WHERE 1");
+		sql.append("AND `relTypeCode` = ?", relTypeCode);
+		sql.append("AND `relId` = ?", relId);
+		sql.append("AND `typeCode` = ?", typeCode);
+		sql.append("AND `type2Code` = ?", type2Code);
 
 		return DBUtil.selectRowStringValue(dbConn, sql);
 	}
 
-	public int remove(String name) {
+	public int remove(String relTypeCode, int relId, String typeCode, String type2Code) {
 		SecSql sql = new SecSql();
 
 		sql.append("DELETE FROM attr");
-		sql.append("WHERE `name` = ?", name);
+		sql.append("WHERE 1");
+		sql.append("AND `relTypeCode` = ?", relTypeCode);
+		sql.append("AND `relId` = ?", relId);
+		sql.append("AND `typeCode` = ?", typeCode);
+		sql.append("AND `type2Code` = ?", type2Code);
 
 		return DBUtil.delete(dbConn, sql);
 	}
